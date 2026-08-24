39eabafca7c118db6898de56e1c889391c49a9dc
Meer Shah <meer@hey.com>
2026-08-24 17:30:30 -0500

    conf to lua

--- hypr/.config/hypr/autostart.lua
+++ hypr/.config/hypr/autostart.lua
@@ -1,2 +1,8 @@
 -- Extra autostart processes.
 -- o.launch_on_start("my-service")
+
+o.launch_on_start("code")
+o.launch_on_start("chromium")
+o.launch_on_start("alacritty")
+o.launch_on_start("spotify")
+o.exec_on_start(o.launch_webapp("https://app.hey.com"))
